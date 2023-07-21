93605fc27d952bb1299aadfae3973bb171e19294
Paul Ged <pauliusgedrimas@gmail.com>
2023-07-21 15:12:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
+# A web based test app to see dynamically built website using a SQL database showcasing cafes based on user based filters
+# Made by Paul Ged
+
+# Import statements
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from random import randrange
 
+# Flask setup
 app = Flask(__name__)
 
-##Connect to Database
+#Connect to Database - currently the app uses a local database in folder
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-##Cafe TABLE Configuration
+# Cafe table Configuration for all the filters
 class Cafe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), unique=True, nullable=False)
@@ -24,17 +29,19 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+# Random cafe output from database
 def random_cafe():
     all_cafes = len(db.session.query(Cafe).all())
     chosen_cafe = Cafe.query.get(randrange(1, all_cafes))
     return chosen_cafe
 
+# homepage
 @app.route('/')
 def home():
     random_cafe()
     return render_template('index.html')
 
-
+# returns JSON object which outputs into HTML
 @app.route('/random')
 def random():
     random_caf = random_cafe()
@@ -53,18 +60,6 @@ def random():
     }}
     return jsonify(cafe_json)
 
-
-
-
-
-## HTTP GET - Read Record
-
-## HTTP POST - Create Record
-
-## HTTP PUT/PATCH - Update Record
-
-## HTTP DELETE - Delete Record
-
-
+# Run from here
 if __name__ == '__main__':
     app.run(debug=True)
